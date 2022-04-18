b273d502aad3a6410ea1e59107b6b81dd3f0f3aa
Hillel Coren <hillelcoren@gmail.com>
2022-04-18 22:02:09 +0300

    Add page count

--- pdf/lib/src/pdf/page_format.dart
+++ pdf/lib/src/pdf/page_format.dart
@@ -27,7 +27,7 @@ class PdfPageFormat {
     double marginLeft = 0.0,
     double marginRight = 0.0,
     double? marginAll,
-    this.pageCount = 0,
+    this.pageCount = 0.0,
   })  : assert(width > 0),
         assert(height > 0),
         marginTop = marginAll ?? marginTop,
